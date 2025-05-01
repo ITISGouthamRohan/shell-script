249e49a7449d8605b3886a6bd777297fa629e1f5
ITISGouthamRohan <gouthamrohan1@gmail.com>
2025-05-01 18:06:31 +0530

    hello-script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -8,4 +8,6 @@ echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "Process ID of the current shell script: $$"
-sleep 60
+# sleep 60 Duplicate the IP in different tab and run ps -ef and check for the process ID that is shown from step -10 command
+sleep 60 &
+echo "Process ID of last backgroung command: $!"
