8f6539483c13ccecd255e8dce96dc7c622f0526d
ITISGouthamRohan <gouthamrohan1@gmail.com>
2025-05-01 16:41:25 +0530

    hello-script

--- 06-Array.sh
+++ 06-Array.sh
@@ -9,3 +9,7 @@ MOVIES=("RRR" "Athadu" "Murari")
 # list always starts with 0.
 
 echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[2]}"
+echo "First Movie is: ${MOVIES[3]}"
+echo "First Movie is: ${MOVIES[@]}"
